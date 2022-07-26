05e923c183f6f184d6773acd7dfcde4cd66e0130
NANO <File changed>
2022-07-27 00:12:58 +0600

    2 submission

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -4,7 +4,6 @@ public class RepeatIsBad {
 
     @SuppressWarnings("java:S106")
     public void repeatIsBad(){
-
         System.out.println("writing the same code doesn't have much impact, and it's also time-consuming");
         System.out.println("writing the same code doesn't have much impact, and it's also time-consuming");
         System.out.println("writing the same code doesn't have much impact, and it's also time-consuming");
